d41c1bc4093915f40d4d326ff5f90d491c49405e
Prisdvl <kakuki@example.com>
2026-09-08 20:31:13 +0800

    deploy: 动画完善版 + 404.html

--- assets/project-JUyviHcJ.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-B9Bg53to.js";const r=()=>t.get("/projects/");export{r as g};
---
+++ assets/project-jc_AnmNI.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-vyKu256a.js";const r=()=>t.get("/projects/");export{r as g};

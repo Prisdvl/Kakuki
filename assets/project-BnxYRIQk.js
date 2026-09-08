bee189618bc4397a63e567e7c12ee08d9bd694b7
Prisdvl <prisdvl@users.noreply.github.com>
2026-09-08 21:37:50 +0800

    feat: 真实歌单静态化/墨染动画/头像同步/播放器布局修复

---
+++ assets/project-BnxYRIQk.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-9IPAvdCg.js";const r=()=>t.get("/projects/");export{r as g};
--- assets/project-mcOieTZB.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-B6K0v-Pt.js";const r=()=>t.get("/projects/");export{r as g};
